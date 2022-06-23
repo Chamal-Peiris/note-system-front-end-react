0bed7141552eecc66257ae2483f79992724427b8
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-06-24 00:16:12 +0530

    Add Note component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Input from "./component/input/Input";
+import Note from "./component/note/Note";
 
-export default class App extends React.Component<any, any>{
+export default class App extends React.Component<any, any> {
 
     render() {
-        return undefined;
+        return (
+            <>
+                <Input/>
+                <Note/>
+                <Note/>
+                <Note/>
+            </>
+        )
     }
 }
---
+++ src/component/note/Note.tsx
@@ -0,0 +1,10 @@
+import './Note.css';
+import React from "react";
+
+export default function Note(){
+   return(
+       <React.Fragment>
+           <h1>Note</h1>
+       </React.Fragment>
+   )
+}
